36e9cf54c24d4be7a593b5741b3c691de152bdb1
uea17 <ma.chacaguasayb@uea.edu.ec>
2024-03-20 12:51:35 -0500

    version deber 3

---
+++ tarea-15.py
@@ -0,0 +1,23 @@
+# Crear un diccionario con información personal
+informacion_personal = {
+    "nombre": "Myrian",
+    "edad": 24,
+    "ciudad": "Riobamba",
+}
+
+# Acceder al valor de "ciudad" y modificarlo
+informacion_personal["ciudad"] = "riobamba"
+
+# Agregar una nueva clave-valor para la profesión
+informacion_personal["profecion"] = "Ingeniero"
+
+# Verificar si la clave "telefono" existe y agregarla si no
+if "telefono" not in informacion_personal:
+    informacion_personal["telefono"] = "123-456-7890"
+
+# Eliminar la clave "edad"
+if "24" in informacion_personal:
+    del informacion_personal["edad"]
+
+# Imprimir el diccionario resultante
+print("Diccionario final:", informacion_personal)
