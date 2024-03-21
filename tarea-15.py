ea216138561f7a7d9e4594e73925dbb50c4c61d6
uea17 <ma.chacaguasayb@uea.edu.ec>
2024-03-21 18:05:52 -0500

    version deber 3

--- tarea-15.py
+++ tarea-15.py
@@ -9,14 +9,14 @@ informacion_personal = {
 informacion_personal["ciudad"] = "riobamba"
 
 # Agregar una nueva clave-valor para la profesión
-informacion_personal["profecion"] = "Ingeniero"
+informacion_personal["profecion"] = "Ingeniera"
 
 # Verificar si la clave "telefono" existe y agregarla si no
 if "telefono" not in informacion_personal:
     informacion_personal["telefono"] = "123-456-7890"
 
 # Eliminar la clave "edad"
-if "24" in informacion_personal:
+if "edad" in informacion_personal:
     del informacion_personal["edad"]
 
 # Imprimir el diccionario resultante
